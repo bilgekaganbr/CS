9393ecd9b1523a2f84f48f762d1b099a363dae63
Bilge Kağan Börekçi <bilgekaganbr@gmail.com>
2022-11-09 17:59:43 +0300

    Update multipleofThree.cs

--- multipleofThree.cs
+++ multipleofThree.cs
@@ -13,17 +13,18 @@ namespace SoloLearn
 		{
 			int number = Convert.ToInt32(Console.ReadLine());
             
-            for (int i = 1; i <= number; i++)
-            {
-                if (i % 3 != 0)
-                {
-                    Console.Write(i);
-                }
-                else
-                {
-                    Console.Write("*");
-                }
-            }            
+            		for (int i = 1; i <= number; i++)
+            		{
+                		if (i % 3 != 0)
+                		{
+                    			Console.Write(i);
+                		}
+                		else
+                		{
+                    			Console.Write("*");
+                		}
+                        
+			}
 		}
 	}
 }
